bd1dcdb2b9ed2190948f2b258350a3ca363c578b
Andrew Knox <andrew.knox.r@gmail.com>
2024-06-11 14:48:56 -0700

    adding onPress cuz now it works bc of changes to App.js

--- components/Button.js
+++ components/Button.js
@@ -8,7 +8,6 @@ export default function Button({ label, theme, onPress }) {
         <Pressable
           style={[styles.button, { backgroundColor: "#fff" }]}
           onPress={onPress}
-          onLongPress={() => alert('You did a long press.')}
         >
           <FontAwesome
             name="picture-o"
@@ -24,7 +23,7 @@ export default function Button({ label, theme, onPress }) {
 
   return (
     <View style={styles.buttonContainer}>
-        <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+        <Pressable style={styles.button} onPress={onPress}>
           <Text style={styles.buttonLabel}>{label}</Text>
         </Pressable>
       </View>
